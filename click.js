f39c864da42745e4da5b75e6f3ff3e8de43dbc40
Hammad214508 <hammadmuhammad15@gmail.com>
2020-09-25 23:23:42 +0100

    basic click event

--- click.js
+++ click.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
+    var total_clicks = 0;
+    var clicks_per_second = 0;
+
+    $.fn.button_click_event = function(){
+        $("#click_btn").on('click', function(){
+            total_clicks += 1;
+            $("#click_count").html(Math.round(total_clicks));
+        });
+    };
+
+    $.fn.clicks_per_second_event = function(){
+        window.setInterval(function(){
+            total_clicks += clicks_per_second;
+            $("#click_count").html(Math.round(total_clicks));
+        }, 1000);
+    }
 
     var pageready = (function(){
         var thispage = {};
         thispage.init = function(){
-            console.log("HERE");
+        $.fn.button_click_event();
+        $.fn.clicks_per_second_event();
+
+        // $("#clicks_per_second").html(clicks_per_second);
+
+
 
         };
         return thispage;
